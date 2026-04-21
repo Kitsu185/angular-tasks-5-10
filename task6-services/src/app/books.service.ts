ce454c963ffdbaa9f15575780536403515fed46b
Kitsu185 <ys.salih2004@gmail.com>
2026-04-21 21:50:13 +0300

    добавил задание6: сервисы

---
+++ task6-services/src/app/books.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BooksService {
+  book = {
+    postId: 1,
+    id: 1,
+    name: 'id labore ex et quam laborum',
+    email: 'Eliseo@gardner.biz',
+    body: `laudantium enim quasi est quidem magnam voluptate ipsam eos
+tempora quo necessitatibus
+dolor quam autem quasi
+reiciendis et nam sapiente accusantium`
+  };
+}
